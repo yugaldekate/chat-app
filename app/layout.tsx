bbf69a017b7ac697e9a7eee05caf60fe1780cfa4
Yugal D Dekate <yugaldekate72@gmail.com>
2024-03-19 00:10:54 +0530

    installed Nextjs, ShadCn, and created grouped folders

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+const font = Open_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Chat App",
+  description: "Build a real-time messaging platform like Discord using Next.js, Socket.io, Prisma, and Tailwind CSS.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
